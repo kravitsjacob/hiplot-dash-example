624775a6eb3a3f262d833682f4c84e5323c88021
Jacob Kravits <kravitsjacob@gmail.com>
2021-09-28 10:50:03 -0600

    Change file name

---
+++ app.py
@@ -0,0 +1,8 @@
+# Example of embedding hiplot into dash
+
+import dash
+import pandas as pd
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+
